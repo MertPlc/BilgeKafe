1fcd420054fd1b7cc931459bfdd2ea3c8a89ef0e
MertPlc <mertpalacioglu@gmail.com>
2021-08-27 17:42:57 +0300

    prop metot tanımları

---
+++ BilgeKafe.Data/KafeVeri.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilgeKafe.Data
+{
+    public class KafeVeri
+    {
+        public int MasaAdet { get; set; }
+
+        public List<Urun> Urunler { get; set; }
+
+        public List<Siparis> AktifSiparisler { get; set; }
+
+        public List<Siparis> GecmisSiparisler { get; set; }
+    }
+}
---
+++ BilgeKafe.Data/Siparis.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilgeKafe.Data
+{
+    public class Siparis
+    {
+        public int MasaNo { get; set; }
+
+        public SiparisDurum Durum { get; set; }
+
+        public decimal OdenenTutar { get; set; }
+
+        public DateTime? AcilisZamani { get; set; } = DateTime.Now;
+        //yukardaki eşit tanımının aynısı
+        //public Siparis()
+        //{
+        //    AcilisZamani = DateTime.Now;
+        //}
+
+        public DateTime? KapanisZamani { get; set; }
+
+        public List<SiparisDetay> SiparisDetaylar { get; set; }
+
+        public string ToplamTutarTL { get; }
+
+        public decimal ToplamTutar()
+        {
+            //decimal toplam = 0;
+            //foreach (SiparisDetay detay in SiparisDetaylar)
+            //{
+            //    toplam += detay.Tutar();
+            //}
+
+            //return toplam;
+
+            return SiparisDetaylar.Sum(x => x.Tutar());  //yukardakının aynısı
+        }
+    }
+}
